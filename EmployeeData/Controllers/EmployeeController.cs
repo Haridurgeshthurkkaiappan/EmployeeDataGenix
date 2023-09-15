c04566bd5ceb5fbf6bd97eb5eeaf0e57538895a3
Hari Durgesh <haridurgesh.thurkkaiappan@gmail.com>
2023-09-15 13:19:37 +0530

    Add project files.

---
+++ EmployeeData/Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeLib.Repo;
+using EmployeeLib.Model;
+
+namespace EmployeeData.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       public EmployeeRepo obj;
+        public EmployeeController()
+        {
+            obj = new EmployeeRepo();
+        }
+        public ActionResult List()
+        {
+            return View("ListEmployeedata", new List<EmployeeModel>(obj.GetEmployeeData()));
+        }
+        public ActionResult Create()
+        {
+            var model = new EmployeeModel();
+            return View("CreateEmployeedata", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeModel data)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    
+                    obj.InsertEmployeeData(data);
+                    return RedirectToAction(nameof(List));
+                }
+                else
+                {
+                    return View("CreateEmployeedata", data);
+
+                }
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+
+        public ActionResult Edit(int Empid)
+        {
+            var result = obj.GetEmployeeData(Empid);
+            return View("EditEmployeeData", result);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int Empid, EmployeeModel data)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                
+                    obj.UpdateEmployeeData(data);
+                    return RedirectToAction(nameof(List));
+                }
+                else
+                {
+                    return View("EditEmployeeData", data);
+
+                }
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+
+        public ActionResult Delete(int Empid)
+        {
+
+            var result = obj.GetEmployeeData(Empid);
+
+            return View("DeleteEmployeedata", result);
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Remove(int Empid)
+        {
+            try
+            {
+                obj.DeleteEmployeeData(Empid);
+                return RedirectToAction(nameof(List));
+             
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ EmployeeLib/Repo/EmployeeRepo.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using EmployeeLib.Model;
+
+namespace EmployeeLib.Repo
+{
+    public class EmployeeRepo
+    {
+        public readonly string connectionString;
+
+
+
+        public EmployeeRepo()
+        {
+            connectionString = @"Data source=DESKTOP-UCPA9BN;Initial catalog=EmployeeData;User Id=sa;Password=Anaiyaan@123";
+        }
+
+        public void InsertEmployeeData(EmployeeModel emp)
+        {
+
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+
+                con.Open();
+                con.Execute($" exec insertemployeedata'{emp.Name}', '{emp.DateOfBirth.ToString("MM-dd-yyyy")}','{emp.Experience}',{emp.Phonenumber},'{emp.EmailAddress}'");
+
+                con.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        public EmployeeModel GetEmployeeData(int id)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                var connection = new SqlConnection(connectionString);
+                con.Open();
+                var Employee = connection.QueryFirst<EmployeeModel>($" exec GetemployeedatabyID {id} ");
+                con.Close();
+
+
+
+                return Employee;
+
+
+            }
+
+            catch (SqlException er)
+            {
+                throw;
+            }
+            catch (Exception r)
+            {
+                throw r;
+            }
+        }
+
+        public List<EmployeeModel> GetEmployeeData()
+        {
+            try
+            {
+                List<EmployeeModel> constrain = new List<EmployeeModel>();
+
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+                constrain = connection.Query<EmployeeModel>($" exec Getemployeedata").ToList();
+                connection.Close();
+
+                return constrain;
+
+
+            }
+
+            catch (SqlException er)
+            {
+                throw;
+            }
+            catch (Exception r)
+            {
+                throw r;
+            }
+        }
+
+        public void UpdateEmployeeData(EmployeeModel emp)
+        {
+            try
+            {
+
+                SqlConnection con = new SqlConnection(connectionString);
+
+                con.Open();
+                con.Execute($" exec Updateemployeedata '{emp.Name}', '{emp.DateOfBirth.ToString("MM-dd-yyyy")}','{emp.Experience}',{emp.Phonenumber},'{emp.EmailAddress}',{emp.Empid}");
+                con.Close();
+            }
+            catch (SqlException eu)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public void DeleteEmployeeData(int id)
+        {
+            try
+            {
+
+
+                SqlConnection con = new SqlConnection(connectionString);
+
+                con.Open();
+                con.Execute($"  exec DeleteEmployeeData { id}");
+
+                con.Close();
+
+            }
+            catch (SqlException ed)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
+
+
+      
